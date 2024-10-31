2ad7d386be51ba1858daba9df830a6f777dbaf81
Matthew Nock <thematt.nock@gmail.com>
2024-10-31 18:46:52 +0000

    Extract planet component

---
+++ PlanetComponent.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class PlanetComponent : Node2D
+{
+	public RandomNumberGenerator RNG = new RandomNumberGenerator();
+	[Export]
+	public PackedScene planetScene;
+	public List<Planet> planets = new List<Planet>();
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+
+		for (int i = 0; i < 8; i++)
+		{
+			Planet planet = planetScene.Instantiate<Planet>();
+			//planet.Radius = 100 + i * 50;
+			planet.Radius = RNG.RandfRange(100, 1000);
+			if (planet.Radius > 332.5 && planet.Radius < 697.5)
+			{
+				planet.mesh.Modulate = new Color(0.6f, 1, 0.6f);
+				planet.habitable = true;
+			}
+			else
+			{
+				planet.mesh.Modulate = new Color(1f, 0.6f, 0.6f);
+			}
+			planet.Length = 50;
+			planet.offset = RNG.RandfRange(0, 2 * Mathf.Pi);
+			planet.Name = "Planet" + i;
+			planet.line.DefaultColor = new Color(RNG.RandfRange(0, 1), RNG.RandfRange(0, 1), RNG.RandfRange(0, 1));
+			AddChild(planet);
+			planets.Add(planet);
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
--- Sun.cs
+++ Sun.cs
@@ -14,27 +14,6 @@ public partial class Sun : Node2D
 	public override void _Ready()
 	{
 		SunMesh.Modulate = new Color(1, 1, 0.4f);
-		for (int i = 0; i < 8; i++)
-		{
-			Planet planet = planetScene.Instantiate<Planet>();
-			//planet.Radius = 100 + i * 50;
-			planet.Radius = RNG.RandfRange(100, 1000);
-			if (planet.Radius > 332.5 && planet.Radius < 697.5)
-			{
-				planet.mesh.Modulate = new Color(0.6f, 1, 0.6f);
-				planet.habitable = true;
-			}
-			else
-			{
-				planet.mesh.Modulate = new Color(1f, 0.6f, 0.6f);
-			}
-			planet.Length = 50;
-			planet.offset = RNG.RandfRange(0, 2 * Mathf.Pi);
-			planet.Name = "Planet" + i;
-			planet.line.DefaultColor = new Color(RNG.RandfRange(0, 1), RNG.RandfRange(0, 1), RNG.RandfRange(0, 1));
-			AddChild(planet);
-			planets.Add(planet);
-		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
